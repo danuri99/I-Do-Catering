ecfa00a0d88947b9b49883b083e7488259f77f04
Danuri <danuri.alwis@gmail.com>
2020-08-16 14:01:26 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/idocatering/RecordDeleted.java
@@ -0,0 +1,14 @@
+package com.example.idocatering;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecordDeleted extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record_deleted);
+    }
+}
